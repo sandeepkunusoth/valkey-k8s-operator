aa9fbea9c7cd4ada5a7a4a12793e503d8f0e485b
Sandeep Kunusoth <sandeepkunsoth000@gmail.com>
2026-08-30 03:40:39 -0500

    fixed comment
    Signed-off-by: Sandeep Kunusoth <sandeepkunsoth000@gmail.com>

--- internal/valkey/version_test.go
+++ internal/valkey/version_test.go
@@ -35,6 +35,7 @@ func TestVersionFromImage(t *testing.T) {
 		{name: "major.minor only", image: "valkey/valkey:9.1", want: "9.1.0", wantOK: true},
 		{name: "distro suffix bookworm", image: "valkey/valkey:9.1.0-bookworm", want: "9.1.0", wantOK: true},
 		{name: "alpine suffix", image: "valkey/valkey:9.1.2-alpine", want: "9.1.2", wantOK: true},
+		{name: "prerelease tag", image: "valkey/valkey:9.1.0-rc1", want: "9.1.0", wantOK: true},
 		{name: "v-prefixed tag", image: "valkey/valkey:v9.1.0", want: "9.1.0", wantOK: true},
 		{name: "tag plus digest", image: "valkey/valkey:9.1.1@sha256:70739f85ad2ee01a726a965584a0f94895f01b0c60b3cc8b0aeef11eaa6888cf", want: "9.1.1", wantOK: true},
 		{name: "registry with port", image: "myregistry:5000/valkey/valkey:9.1.0", want: "9.1.0", wantOK: true},
